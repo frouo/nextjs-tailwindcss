eda0fcce544fff6b7c70dd8dcf1a5df8e170ed44
frouo <francois.rouault@cocoricostudio.com>
2023-04-20 20:12:03 +0200

    wip: Web app layout

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,21 @@
 export default function Home() {
-  return <div>Hello world!</div>;
+  return (
+    <>
+      <div className="p-2 bg-gray-200 text-center">top banner</div>
+      <div className="flex-1 flex">
+        <div className="w-24 bg-gray-600 flex flex-col justify-between text-center">
+          <div className="py-4">
+            <p>contact</p>
+            <p>inbox</p>
+          </div>
+          <div className="pb-4">settings</div>
+        </div>
+        <div className="flex-1 overflow-y-scroll p-2">
+          {Array.from(Array(100).keys()).map((e) => (
+            <p key={e}>{e}</p>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
